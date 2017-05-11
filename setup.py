a39f4e58f81ae3fd5a6d2e74a202bc46efe8b7ad
Bo Laurent <bo@bolaurent.com>
2017-05-11 14:14:41 -0700

    dependency_links

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     url = "",
     install_requires=[
         "cmd2",
-        "zuora_restful_python"
+        "zuora_restful_python==0.13-dev0"
     ],
     dependency_links=['http://github.com/bolaurent/zuora_restful_python.git@master#egg=zuora_restful_python-0.13-dev0']
     )
